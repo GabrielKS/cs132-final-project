4de27207ebaf5fcc8b43eb17b976851ff2e30994
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2022-04-29 16:28:33 -0700

    Implement a simple lexer for the language

---
+++ Lexer.hs
@@ -0,0 +1,23 @@
+module Lexer where
+import Language
+
+getTokens :: String -> [Token]
+getTokens "" = []
+getTokens s@(h:t)
+    | h == '(' = TLParen : getTokens t
+    | h == ')' = TRParen : getTokens t
+    | h == '+' = TPlus : getTokens t
+    | isWhitespace h = getTokens t
+    | otherwise = let (thisIdent, rest) = getIdent s in thisIdent : getTokens rest
+
+isWhitespace :: Char -> Bool
+isWhitespace c = c `elem` [' ', '\t', '\n']
+isReserved :: Char -> Bool
+isReserved c = c `elem` ['(', ')', '+']
+isIdent :: Char -> Bool
+isIdent c = not (isWhitespace c) && not (isReserved c)
+
+getIdent input = let (soFar, rest) = getIdentHelper "" input in (TIdent (reverse soFar), rest)
+getIdentHelper :: String -> String -> (String, String)
+getIdentHelper soFar "" = (soFar, "")
+getIdentHelper soFar s@(h:t) = if isIdent h then getIdentHelper (h : soFar) t else (soFar, s)
