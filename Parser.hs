55a8c00423c621375cc24086cd855a80e6743eed
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2022-05-01 19:02:36 -0700

    Extract sequence of productions during parse
    Beginning work on a parser that will return an AST. Here, we do some
    refactoring to keep track of the production rules the recognizer is using.

---
+++ Parser.hs
@@ -0,0 +1,122 @@
+{-# LANGUAGE LambdaCase #-}
+module Parser(parse) where
+import Language
+import Lexer
+import Debug.Trace
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+-- Toggle debug printing
+dtrace = trace  -- On
+-- dtrace _ fun = fun  -- Off
+
+data Result = Accept | Reject String deriving Show  -- All happy families are alike...
+data Progress = Nopop | Pop Int Nonterm | End Result deriving Show  -- The various messages states need to pass up the chain
+type StateFn = Progress -> [Token] -> [Token] -> [Production] -> (Progress, [Token], [Token], [Production])  -- States are of this type
+data Element = ET Token | ENT Nonterm deriving Show
+type Production = (Nonterm, [Element])
+productions :: [[Token] -> Production]
+productions = [
+    const (E', [ENT E]),
+    const (E, [ENT E, ET TPlus, ENT T]),
+    const (E, [ENT T]),
+    const (T, [ET TLParen, ENT E, ET TRParen]),
+    \case
+        (TIdent s) : _ -> (T, [ET (TIdent s)])
+        _ -> error "Malformed parser; should be encountering a TIdent here"
+    ]
+
+ts = getTokens "a+(b)"  -- Test string
+
+parse :: [Token] -> Expr
+parse = undefined
+
+recognize :: [Token] -> Result  -- The main function of import (no pun intended). Call it with some tokens and it tells you whether you have a string in the language.
+recognize tokens = case state0 Nopop tokens [] [] of
+    (End res, remnants, done, theParse) -> dtrace (show theParse) res
+    (_, remnants, done, theParse) -> Reject $ "Exited parser with remaining tokens " ++ show remnants ++ " and parse " ++ show theParse
+
+-- Some debug helper functions
+printVars nToPop nt restTokens statename = dtrace ("\tnToPop=" ++ show nToPop ++ ", nt=" ++ show nt ++ ", state=" ++ show statename)
+printStateStart statename tokens = dtrace ("state=" ++ show statename ++ ", tokens=" ++ show tokens)
+
+-- All shift states are of this form
+shiftConstructor :: String -> (Token -> Maybe StateFn) -> (Nonterm -> Maybe StateFn) -> StateFn
+shiftConstructor _ _ _ (End res) tokens done theParse = (End res, tokens, done, theParse)
+shiftConstructor statename _ _ _ [] done theParse = (End (Reject ("Empty input to shift state " ++ statename)), [], done, theParse)
+shiftConstructor statename actionmap gotomap _ tokens@(h:t) done theParse = printStateStart statename tokens $ 
+    let
+        (prog, restTokens, newDone, newParse) = case actionmap h of
+            Nothing -> (End (Reject ("No match for terminal " ++ show h ++ " in state " ++ statename)), tokens, done, theParse)
+            Just f -> f Nopop t (h:done) theParse
+    in
+    handleGoto statename gotomap (prog, restTokens, newDone, newParse)
+
+-- Helper function to handle the recursion involved in goto-ing
+handleGoto :: String -> (Nonterm -> Maybe StateFn) -> (Progress, [Token], [Token], [Production]) -> (Progress, [Token], [Token], [Production])
+handleGoto _ _ (End res, tokens, done, theParse) = (End res, tokens, done, theParse)
+handleGoto _ _ (Nopop, tokens, done, theParse) = error $ "Malformed parser; should not be encountering a Nopop here. Tokens = " ++ show tokens
+handleGoto statename gotomap (Pop nToPop nt, restTokens, done, theParse) = printVars nToPop nt restTokens statename $
+            if nToPop > 0 then
+                (Pop (nToPop-1) nt, restTokens, done, theParse)
+            else
+                case gotomap nt of
+                    Nothing -> (End (Reject ("No match for non-terminal " ++ show nt ++ " in state " ++ statename)), restTokens, done, theParse)
+                    Just f -> handleGoto statename gotomap (f Nopop restTokens done theParse)
+
+-- All reduce states are of this form
+reduceConstructor :: String -> Nonterm -> Int -> ([Token] -> Production) -> StateFn
+reduceConstructor _ _ _ _ (End res) tokens done theParse = (End res, tokens, done, theParse)
+reduceConstructor statename nt nToPop myProduction _ tokens done theParse = printStateStart statename tokens (Pop (nToPop-1) nt, tokens, done, myProduction done : theParse)
+
+state0 :: StateFn
+state0 = shiftConstructor "0"
+    (\case
+        TIdent _ -> Just state4
+        TLParen  -> Just state3
+        _ -> Nothing)
+    (\elem -> Map.lookup elem (Map.fromList [(E, state1), (T, state2)]))
+    
+state1 :: StateFn
+state1 = shiftConstructor "1"
+    (\case
+        TPlus -> Just state5
+        TEnd  -> Just (\_ tokens done theParse -> (End Accept, tokens, done, theParse))  -- Function that tells shiftConstructor to accept
+        _ -> Nothing)
+    (const Nothing)
+
+state2 :: StateFn
+state2 = reduceConstructor "2" E 1 (productions !! 2)
+
+state3 :: StateFn
+state3 = shiftConstructor "3"
+    (\case
+        TIdent _ -> Just state4
+        TLParen  -> Just state3
+        _ -> Nothing)
+    (\elem -> Map.lookup elem (Map.fromList [(E, state6), (T, state2)]))
+
+state4 :: StateFn
+state4 = reduceConstructor "4" T 1 (productions !! 4)
+
+state5 :: StateFn
+state5 = shiftConstructor "5"
+    (\case
+        TIdent _ -> Just state4
+        TLParen  -> Just state3
+        _ -> Nothing)
+    (\elem -> Map.lookup elem (Map.fromList [(T, state8)]))
+
+state6 :: StateFn
+state6 = shiftConstructor "6"
+    (\case
+        TPlus   -> Just state5
+        TRParen -> Just state7
+        _ -> Nothing)
+    (const Nothing)
+
+state7 :: StateFn
+state7 = reduceConstructor "7" T 3 (productions !! 3)
+
+state8 :: StateFn
+state8 = reduceConstructor "8" E 3 (productions !! 1)
